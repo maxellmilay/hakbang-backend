c0aaca73005efef4042e31bcf6b00b653b229876
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-10-17 17:49:51 +0800

    add urls and views

---
+++ accounts/serializers.py
@@ -0,0 +1,73 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from accounts.models import CustomUser, Organization
+
+from annotation.serializers import FileSerializer
+from annotation.models import File
+
+
+class OrganizationSerializer(serializers.ModelSerializer):
+    created_on = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True)
+    profile_picture = FileSerializer(read_only=True)
+    profile_picture_id = serializers.IntegerField(write_only=True, required=False)
+
+    class Meta:
+        model = Organization
+        fields = ['id', 'name', 'created_on', 'profile_picture', 'profile_picture_id']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(read_only=True)
+    organization = OrganizationSerializer(read_only=True)
+    organization_id = serializers.IntegerField(write_only=True, required=False)
+    profile_picture = FileSerializer(read_only=True)
+    profile_picture_id = serializers.IntegerField(write_only=True, required=False)
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'is_admin', 'full_name', 'organization', 'organization_id', 'profile_picture', 'profile_picture_id')
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    organization_id = serializers.IntegerField(write_only=True, required=False)
+    profile_picture_id = serializers.IntegerField(write_only=True, required=False)
+
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'password', 'email', 'first_name', 'last_name', 'is_admin', 'organization_id', 'profile_picture_id')
+
+    def create(self, validated_data):
+        organization_id = validated_data.pop('organization_id', None)
+        profile_picture_id = validated_data.pop('profile_picture_id', None)
+        if organization_id:
+            organization = Organization.objects.get(id=organization_id)
+        if profile_picture_id:
+            profile_picture = File.objects.get(id=profile_picture_id)
+        
+
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            is_admin=validated_data.get('is_admin', False),
+            organization=organization or None,
+            profile_picture=profile_picture or None
+        )
+        return user
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['user'] = UserSerializer(self.user).data
+        return data
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import RegisterSerializer, CustomTokenObtainPairSerializer, UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import CustomUser
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+class UserProfileView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
