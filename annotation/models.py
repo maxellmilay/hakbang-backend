0e85500dc8e340288bc2ae82e98f0551ea731435
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-10-21 00:27:46 +0800

    fixbug

--- annotation/models.py
+++ annotation/models.py
@@ -145,4 +145,4 @@ class AnnotationImage(models.Model):
     annotation = models.ForeignKey(Annotation, on_delete=models.CASCADE, related_name='images')
 
     def __str__(self):
-        return self.image.url
+        return self.file.url
