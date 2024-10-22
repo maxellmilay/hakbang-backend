7c299ee1497d769526289caa41767e9534b82beb
Maxell Milay <milaymaxell@gmail.com>
2024-10-22 11:03:00 +0800

    [Adjustment][Maxell] fixed duplicate calculate accessibility score

--- annotation/apps.py
+++ annotation/apps.py
@@ -1,12 +1,17 @@
 from django.apps import AppConfig
 import threading
+import os
 from annotation.utils import calculate_accessibility_score
 
 
 class AnnotationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'annotation'
+    thread_started = False
 
     def ready(self):
-        thread = threading.Thread(target=calculate_accessibility_score, daemon=True)
-        thread.start()
+        # Only run the thread in the main process, not in autoreload processes
+        if os.environ.get('RUN_MAIN') and not self.thread_started:
+            self.thread_started = True
+            thread = threading.Thread(target=calculate_accessibility_score, daemon=True)
+            thread.start()
--- annotation/utils/accessibility_score.py
+++ annotation/utils/accessibility_score.py
@@ -4,7 +4,7 @@ import time
 def calculate_accessibility_score():
     while True:
         print("Calculating accessibility score...")
+
         #logic here
 
-        time.sleep(60*60*60)
-        # time.sleep(5)
+        time.sleep(60*60)
