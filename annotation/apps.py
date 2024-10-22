6da5ac17cb884318e8109c228a871668ff474218
cup-noodlehS <sheldonarthursagrado@gmail.com>
2024-10-22 08:31:03 +0800

    add cron job

--- annotation/apps.py
+++ annotation/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+import threading
+from annotation.utils import calculate_accessibility_score
 
 
 class AnnotationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'annotation'
+
+    def ready(self):
+        thread = threading.Thread(target=calculate_accessibility_score, daemon=True)
+        thread.start()
