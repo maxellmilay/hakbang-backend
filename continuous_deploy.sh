aefbdf4f43b4fe1e614e460de109163ee8b92d41
Maxell Milay <milaymaxell@gmail.com>
2024-10-23 10:08:15 +0800

    [Deployment][Maxell] added CD for github actions

---
+++ continuous_deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Set default region for Cloud Run (optional)
+gcloud config set run/region us-central1
+
+# Variables
+PROJECT_ID="lakbai-439310"
+LOCATION="us-central1"
+REPOSITORY="lakbai"
+IMAGE="lakbai-backend"
+TAG="latest"
+
+IMAGE_NAME=$LOCATION-docker.pkg.dev/$PROJECT_ID/$REPOSITORY/$IMAGE
+IMAGE_URI=$IMAGE_NAME:$TAG
+
+# Build Docker image
+docker build -t $IMAGE_URI .
+
+# Push the Docker image to Google Artifact Registry
+docker push $IMAGE_URI
+
+# Deploy to Cloud Run
+gcloud run deploy lakbai-backend \
+  --image=$IMAGE_URI \
+  --platform=managed \
+  --region=us-central1 \
+  --allow-unauthenticated
